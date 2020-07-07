8c2c345954095fcade99ba1d95f4440d4681126c
ThWhMn <52445096+ThWhMn@users.noreply.github.com>
2020-07-07 22:50:28 +0800

    Update common.h

--- common.h
+++ common.h
@@ -11,7 +11,7 @@ int socket_create(int port);
 void make_block(int fd);
 void make_non_block(int fd);
 #ifdef _D
-#define DBG(fmt, args...) printf(fmt, ##argc)
+#define DBG(fmt, args...) printf(fmt, ##args)
 #else
 #define DBG(fmt, args...)
 #endif
